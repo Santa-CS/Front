b1b3ac32424ca5083710dd4438872ef56f04a804
개발바닥용발바닥 <114556143+dogpowdragonpow@users.noreply.github.com>
2023-02-24 14:54:57 +0900

    이메일페이지 수정

--- src/pages/Email.js
+++ src/pages/Email.js
@@ -1,23 +1,44 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 
+/*
 const User={
     id:'test@example.com',
     pw:'test123!'
-}
+}*/
+
 
 const Email = (props) => {
 
-    const[id,setid]=useState('');
-    const[pw,setpw]=useState('');
+    const[id,setid]=useState("");
+    const[pw,setpw]=useState("");
 
     const confirm=()=>{
-        if(id===User.id && pw===User.pw){
+        fetch('http://ec2-3-35-151-197.ap-northeast-2.compute.amazonaws.com/api/login',{
+            method:'POST',
+            body:JSON.stringify({
+                email:id,
+                password:pw,
+            }),
+        })
+            .then(res=>res.json())
+            .then((result)=>{
+                if(result.token!==undefined){
+                    alert('성공');
+                    localStorage.setItem('access_token :',result.token);
+                    Navigate('/MainP');
+                }
+                else{
+                    alert('아이디 또는 패스워드를 확인해주세요');
+                }
+                console.log('결과 : ',result);
+            });
+       /*if(id===User.id && pw===User.pw){
             window.location.href='/MainP';
         }
         else{
             alert('noopp.');
-        }
+        }*/
     }
 
     return (
