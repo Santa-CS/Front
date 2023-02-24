dd8b32e52276f55d3f8a2f658f0dab2e4f9be7c3
개발바닥용발바닥 <114556143+dogpowdragonpow@users.noreply.github.com>
2023-02-24 15:37:33 +0900

    .

--- src/pages/Solve.js
+++ src/pages/Solve.js
@@ -2,18 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Solve = (props) => {
-    return (
-        <div class="frame1">
-            <div class="frame2">
-                <p>Q1. 선택하세요</p>
-                <button>A</button>
-                <button>B</button>
-                <button>C</button>
-                <button>D</button>
-                <button>다음 문제로</button>
-            </div>
-        </div>
-    );
+    const confirm=()=>{
+        fetch('http://ec2-3-35-151-197.ap-northeast-2.compute.amazonaws.com/api/questions/new',{
+            method:'POST',
+            body:JSON.stringify({
+            }),
+        })
+            .then(res=>res.json())
+            .then((result)=>{
+                
+            });
+    }
 }
 
 export default Solve;
